f49eac39d430773c00eeaf0adeef983e2bdd6086
Favela6 <68201231+Favela6@users.noreply.github.com>
2021-08-18 19:49:20 -0500

    Create userAPI.ts

---
+++ src/app/interfaces/userAPI.ts
@@ -0,0 +1,16 @@
+export interface UserResult{
+    page:number;
+    per_page:number;
+    total:number;
+    total_pages:number;
+    data:User[];
+}
+
+
+export interface User{
+    id:number;
+    email:string;
+    first_name:string;
+    last_name:string;
+    avatar:string;
+}
